6ef5341f6d69aab3372d08fa0348c3fc6c228d8d
zimzaza4 <1875162740@qq.com>
2023-02-18 23:18:46 +0800

    null check

--- src/main/java/re/imc/geyserchestuiadapter/GeyserChestUIAdapter.java
+++ src/main/java/re/imc/geyserchestuiadapter/GeyserChestUIAdapter.java
@@ -1,21 +1,18 @@
 package re.imc.geyserchestuiadapter;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import dev.simplix.protocolize.api.Direction;
-import dev.simplix.protocolize.api.Location;
 import dev.simplix.protocolize.api.Protocolize;
 import dev.simplix.protocolize.api.listener.AbstractPacketListener;
 import dev.simplix.protocolize.api.listener.PacketReceiveEvent;
 import dev.simplix.protocolize.api.listener.PacketSendEvent;
 import dev.simplix.protocolize.data.packets.OpenWindow;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.ComponentSerializer;
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
-import org.geysermc.floodgate.api.FloodgateApi;
 import org.geysermc.floodgate.api.InstanceHolder;
 import org.slf4j.Logger;
 
@@ -36,9 +33,10 @@ public class GeyserChestUIAdapter {
 
     @Subscribe
     public void onProxyInitialization(final ProxyInitializeEvent event) {
-        Protocolize.listenerProvider().registerListener(new AbstractPacketListener<OpenWindow>(OpenWindow.class, Direction.UPSTREAM, 0) {
+
+        Protocolize.listenerProvider().registerListener(new AbstractPacketListener<>(OpenWindow.class, Direction.UPSTREAM, 0) {
             public void packetReceive(PacketReceiveEvent<OpenWindow> packetReceiveEvent) {
-                
+
             }
 
             public void packetSend(PacketSendEvent<OpenWindow> packetSendEvent) {
@@ -51,10 +49,11 @@ public class GeyserChestUIAdapter {
                         case GENERIC_9X4 -> appendTitle = "chest.row.4";
                         case GENERIC_9X5 -> appendTitle = "chest.row.5";
                     }
+                    if (appendTitle != null) {
+                        Component title = SERIALIZER.deserialize(packet.titleJson());
+                        packet.titleJson(SERIALIZER.serialize(title.append(Component.text(appendTitle))));
 
-                    Component title = SERIALIZER.deserialize(packet.titleJson());
-                    packet.titleJson(SERIALIZER.serialize(title.append(Component.text(appendTitle))));
-                    packetSendEvent.packet(packet);
+                    }
                 }
 
             }
